aa79c33d1528a1a910fab00602056eeb55453375
DELL <DELL@DESKTOP-POSCU5R>
2022-01-07 15:19:05 +0530

    Addition in java

---
+++ addition/src/addition/Shubhuuuu.java
@@ -0,0 +1,9 @@
+package addition;
+
+public class Shubhuuuu {
+public static void main(String[] args){
+	int x=20,y=10,Sum;
+	Sum=x+y;
+	System.out.println("Sum is: "+Sum);
+}
+}
---
+++ addition/src/addition/increment.java
@@ -0,0 +1,10 @@
+package addition;
+
+public class increment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
